f9c2d6887779828f5dab7f8fb786aee70faec356
shamil <shahmardanov.shamil@gmailcom>
2024-06-18 13:47:02 +0400

    first commit

---
+++ src/main/java/com/example/hilay_app/HilayAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hilay_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HilayAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hilay_app/HilayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hilay_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HilayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
