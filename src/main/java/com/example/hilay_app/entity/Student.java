d50b55e5784ab6c9eb2ad00addf16942c6684505
Huseyn <59554521+huseynyusif@users.noreply.github.com>
2024-06-23 19:12:02 +0400

    added Student Entity

---
+++ src/main/java/com/example/hilay_app/entity/Student.java
@@ -0,0 +1,37 @@
+package com.example.hilay_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+    private String groupName;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+
+}
