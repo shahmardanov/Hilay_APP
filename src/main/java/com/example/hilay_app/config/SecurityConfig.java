9acca6d693c6b2988c803801b9b45d04ca3ea954
Huseyn <59554521+huseynyusif@users.noreply.github.com>
2024-06-26 02:35:39 +0400

    added security config and CustomUserDetailsService

---
+++ src/main/java/com/example/hilay_app/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.hilay_app.config;
+
+import com.example.hilay_app.entity.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/**")
+                                .permitAll())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/example/hilay_app/repository/StudentRepository.java
+++ src/main/java/com/example/hilay_app/repository/StudentRepository.java
@@ -2,8 +2,17 @@ package com.example.hilay_app.repository;
 
 import com.example.hilay_app.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    List<Student> findByFkGroupId(Long fkGroupId);
+    Student findByUsername(String username);
+
+    @Query("SELECT s FROM Student s JOIN ChatGroup g ON s.fkGroupId = g.id WHERE g.name = :groupName")
+    List<Student> findByGroupName(@Param("groupName") String groupName);
 }
