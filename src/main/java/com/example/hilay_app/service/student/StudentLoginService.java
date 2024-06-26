ff670d7ee8d5da721f4eaeb9b3790efe2dd6c89b
Aytac <aramazanli@std.beu.edu.az>
2024-06-26 16:36:10 +0400

    added StudentLoginService

---
+++ src/main/java/com/example/hilay_app/service/student/StudentLoginService.java
@@ -0,0 +1,19 @@
+package com.example.hilay_app.service.student;
+
+import com.example.hilay_app.request.login.StudentLoginRequest;
+import com.example.hilay_app.response.login.StudentLoginResponse;
+import com.example.hilay_app.service.security.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentLoginService {
+
+    private final AuthenticationService authenticationService;
+
+    public StudentLoginResponse login(StudentLoginRequest loginRequest) {
+        return authenticationService.authenticateAndGetLoginResponse(loginRequest);
+    }
+
+}
