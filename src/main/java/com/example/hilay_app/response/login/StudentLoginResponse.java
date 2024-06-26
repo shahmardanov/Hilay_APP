141c0ff6f7ca73a276b509b40dac5b16d838f362
Aytac <aramazanli@std.beu.edu.az>
2024-06-26 16:35:51 +0400

    added StudentLoginResponse

---
+++ src/main/java/com/example/hilay_app/response/login/StudentLoginResponse.java
@@ -0,0 +1,18 @@
+package com.example.hilay_app.response.login;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class StudentLoginResponse {
+
+    private String username;
+    private String password;
+
+}
