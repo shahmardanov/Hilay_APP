e0ff56d3a049e3c0618c1aaa6440654321ddbab3
Aytac <aramazanli@std.beu.edu.az>
2024-06-26 22:12:40 +0400

    added AuthenticationManager method

--- src/main/java/com/example/hilay_app/config/SecurityConfig.java
+++ src/main/java/com/example/hilay_app/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.hilay_app.config;
 import com.example.hilay_app.entity.Student;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
@@ -35,4 +37,10 @@ public class SecurityConfig{
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
