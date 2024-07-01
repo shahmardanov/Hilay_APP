0dce413d843bfe8ff4b88428379df73f2055c32b
Aytac <aramazanli@std.beu.edu.az>
2024-07-01 18:56:45 +0400

    added  QuestionsLevelCreateRequest

---
+++ src/main/java/com/example/hilay_app/request/create/QuestionsLevelCreateRequest.java
@@ -0,0 +1,13 @@
+package com.example.hilay_app.request.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionsLevelCreateRequest {
+    private String difficulty;
+}
