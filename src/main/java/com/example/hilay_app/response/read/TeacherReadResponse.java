b97142bbcc1842676a321c449538f71ed64c2d68
Aytac <aramazanli@std.beu.edu.az>
2024-07-01 19:05:50 +0400

    added  TeacherReadResponse

---
+++ src/main/java/com/example/hilay_app/response/read/TeacherReadResponse.java
@@ -0,0 +1,16 @@
+package com.example.hilay_app.response.read;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeacherReadResponse {
+
+
+    private String name;
+
+}
