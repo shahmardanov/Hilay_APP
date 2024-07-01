455103ea911bb7ef3f3ca081da008f380f4f08ea
Aytac <aramazanli@std.beu.edu.az>
2024-07-01 18:58:11 +0400

    added  QuestionsLevelReadResponse

---
+++ src/main/java/com/example/hilay_app/response/read/QuestionsLevelReadResponse.java
@@ -0,0 +1,13 @@
+package com.example.hilay_app.response.read;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionsLevelReadResponse {
+    private String difficulty;
+}
