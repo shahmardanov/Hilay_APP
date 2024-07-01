6f77e6577c3e074fcbc9478a22063b2141462b76
Aytac <aramazanli@std.beu.edu.az>
2024-07-01 18:28:34 +0400

    added  MainCategoryUpdateResponse

---
+++ src/main/java/com/example/hilay_app/response/update/MainCategoryUpdateResponse.java
@@ -0,0 +1,14 @@
+package com.example.hilay_app.response.update;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainCategoryUpdateResponse {
+
+    private String mainCategoryName;
+}
