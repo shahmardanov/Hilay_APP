df81727315df11b48d3235fa514964b61ec3847b
Aytac <aramazanli@std.beu.edu.az>
2024-07-01 19:06:50 +0400

    added  TeacherUpdateRequest

---
+++ src/main/java/com/example/hilay_app/request/update/TeacherUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.example.hilay_app.request.update;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeacherUpdateRequest {
+
+
+    private String name;
+
+}
