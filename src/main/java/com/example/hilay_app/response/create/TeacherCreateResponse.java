cfab4539aaf1401e5c99e253c636b77431bcf948
Aytac <aramazanli@std.beu.edu.az>
2024-07-01 19:04:46 +0400

    added  TeacherCreateResponse

---
+++ src/main/java/com/example/hilay_app/response/create/TeacherCreateResponse.java
@@ -0,0 +1,16 @@
+package com.example.hilay_app.response.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeacherCreateResponse {
+
+
+    private String name;
+
+}
