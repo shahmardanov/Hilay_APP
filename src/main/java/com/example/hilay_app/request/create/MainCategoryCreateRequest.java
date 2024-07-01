f680df0891671170f05935555f136ed885f3a104
Aytac <aramazanli@std.beu.edu.az>
2024-07-01 18:21:14 +0400

    added MainCategoryCreateRequest

---
+++ src/main/java/com/example/hilay_app/request/create/MainCategoryCreateRequest.java
@@ -0,0 +1,14 @@
+package com.example.hilay_app.request.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainCategoryCreateRequest {
+
+    private String mainCategoryName;
+}
