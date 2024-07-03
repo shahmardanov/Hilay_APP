aef1cac87c6ed795105392816a2c02b93d34ebb7
Aytac <aramazanli@std.beu.edu.az>
2024-07-03 19:35:44 +0400

    added ImageMapper

--- src/main/java/com/example/hilay_app/mapper/ImageMapper.java
+++ src/main/java/com/example/hilay_app/mapper/ImageMapper.java
@@ -1,9 +1,6 @@
 package com.example.hilay_app.mapper;
 
-import com.example.hilay_app.entity.Image;
-import com.example.hilay_app.response.read.ImageReadResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
@@ -11,13 +8,7 @@ import org.mapstruct.ReportingPolicy;
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface ImageMapper {
 
-    Image toEntity(ImageCreateRequest request);
-  //  ImageCreateResponse toResponseFromEntity(Image entity);
 
-    ImageReadResponse entityToImageReadResponse(Image image);
-
-    void mapForUpdate(@MappingTarget Image image, ImageUpdateRequest imageUpdateRequest);
-    ImageUpdateResponse toUpdateResponse(Image image);
 
 
 }
