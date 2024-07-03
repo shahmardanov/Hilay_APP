43f4a3417af762f511544223d3f7c776e57910ee
Aytac <aramazanli@std.beu.edu.az>
2024-07-03 19:39:17 +0400

    added ImageRepository

--- src/main/java/com/example/hilay_app/repository/ImageRepository.java
+++ src/main/java/com/example/hilay_app/repository/ImageRepository.java
@@ -4,6 +4,10 @@ import com.example.hilay_app.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
+    Optional<Image> findByFileName(String fileName);
+
 }
